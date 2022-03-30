eeb29524c7160c6be458f37738d16712c03f4525
Selenbp1 <akfkdhwk@gmail.com>
2022-03-30 17:28:50 +0900

    지하철 시간대별 데이터 시각화하기 & 숫자 데이터를 쉽게 다루게 돕는 numpy 라이브러리

---
+++ unit12-01.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import csv
+f = open('subwaytime.csv' , 'r', encoding='utf8')
+data = csv.reader(f)
+for row in data :
+    print(row)
+
+
+# In[13]:
+
+
+import csv
+f = open('subwaytime.csv' , 'r', encoding='utf8')
+data = csv.reader(f)
+next(data)
+next(data)
+for row in data :
+    row[4:] = map(int,row[4:])
+    print(row)
+
+
+# In[15]:
+
+
+import csv
+f = open('subwaytime.csv' , 'r', encoding='utf8')
+data = csv.reader(f)
+next(data)
+next(data)
+result=[]
+for row in data :
+    row[4:] = map(int,row[4:])
+    result.append(row[10])
+    print(len(result))
+    print(result)
+
+
+# In[16]:
+
+
+import matplotlib.pyplot as plt
+plt.bar(range(len(result)), result)
+plt.show()
+
+
+# In[17]:
+
+
+import matplotlib.pyplot as plt
+result.sort()  # 오름차순으로 정렬
+plt.bar(range(len(result)), result)
+plt.show()
+
+
+# In[20]:
+
+
+import csv
+f = open('subwaytime.csv' , 'r', encoding='utf8')
+data = csv.reader(f)
+next(data)
+next(data)
+mx = 0               # 최댓값을 저장할 변수 초기화
+mx_station = ''     # 최댓값을 갖는 역 이름 저장 변수 초기화
+for row in data :    # 최댓값 찾기(전부 탐색하여 최댓값을 갱신하는 방식)
+    row[4:] = map(int, row[4:])
+    if sum(row[10:15:2]) > mx :
+        mx = sum(row[10:15:2])
+        mx_station = row[3]+'('+row[1]+')'
+print(mx_station, mx)
+
+
+# In[21]:
+
+
+import csv
+f = open('subwaytime.csv' , 'r', encoding='utf8')
+data = csv.reader(f)
+next(data)
+next(data)
+mx = 0               # 최댓값을 저장할 변수 초기화
+mx_station = ''     # 최댓값을 갖는 역 이름 저장 변수 초기화
+for row in data :    # 최댓값 찾기(전부 탐색하여 최댓값을 갱신하는 방식)
+    row[4:] = map(int, row[4:])
+    a = row[11:16:2]    #하차 인원 값 추출하기
+    if sum(a) > mx :
+        mx = sum(a)
+        mx_station = row[3]+'('+row[1]+')'
+print(mx_station, mx)
+
+
+# In[27]:
+
+
+import csv
+f = open('subwaytime.csv' , 'r', encoding='utf8')
+data = csv.reader(f)
+next(data)
+next(data)
+mx = 0   
+mx_station = ''
+t = int(input('몇 시의 승차 인원이 가장 많은 역이 궁금하세요? : '))
+                                
+for row in data :
+    row[4:] = map(int, row[4:])
+    a = row[4+(t-4)*2]          # 입력 받은 시각의 승차 인원 값 추출하기
+    if a > mx :                 # 모든 데이터 탐색
+        mx = a
+        mx_station = row[3]+'('+ row[1]+')'
+print(mx_station, mx)           # 승차 인원이 가장 큰 역과 인원 값 출력
+
+
+# In[7]:
+
+
+import csv
+f = open('subwaytime.csv', 'r', encoding='utf8')
+data = csv.reader(f)
+next(data)
+next(data)
+mx = [0]*24
+mx_station = ['']*24
+for row in data :
+    row[4:]=map(int,row[4:])
+    for j in range(24) :
+        a=row[j*2+4]
+        if a>mx[j]:
+                mx[j] = a
+                mx_station[j] = row[3]+'(' + str(j+4) + '시)'
+print(mx_station)
+print(mx)
+
+
+# In[9]:
+
+
+import csv
+import matplotlib.pyplot as plt
+f = open('subwaytime.csv', 'r', encoding='utf8')
+data = csv.reader(f)
+next(data)
+next(data)
+mx = [0]*24 #시간대별 최대 승차 인원을 저장할 리스트 초기화
+mx_station = ['']*24 #시간대별 최대 승차 인원 역 이름을 저장할 리스트 초기화
+for row in data :
+    row[4:]=map(int,row[4:])
+    for j in range(24) :
+        a=row[j*2+4] #j값과 인데스 번호 값의 관계식 사용
+        if a>mx[j]:
+                mx[j] = a
+                mx_station[j] = row[3]+'(' + str(j+4) + '시)'
+plt.rc('font',family='AppleGothic')
+plt.bar(range(24),mx, color='b') # 막대그래프 속성 변경
+plt.xticks(range(24),mx_station, rotation=90)
+plt.show()
+
+
+# In[10]:
+
+
+import csv
+import matplotlib.pyplot as plt
+f = open('subwaytime.csv', 'r', encoding='utf8')
+data = csv.reader(f)
+next(data)
+next(data)
+mx = [0]*24 #시간대별 최대 하차 인원을 저장할 리스트 초기화
+mx_station = ['']*24 #시간대별 최대 하차 인원 역 이름을 저장할 리스트 초기화
+for row in data :
+    row[4:]=map(int,row[4:])
+    for j in range(24) :
+        a=row[j*2+5] #j값과 인데스 번호 값의 관계식 사용
+        if a>mx[j]:
+                mx[j] = a
+                mx_station[j] = row[3]+'(' + str(j+4) + '시)'
+plt.rc('font',family='AppleGothic')
+plt.bar(range(24),mx, color='b') # 막대그래프 속성 변경
+plt.xticks(range(24),mx_station, rotation=90)
+plt.show()
+
+
+# In[15]:
+
+
+import csv
+import matplotlib.pyplot as plt 
+
+f = open('subwaytime.csv', 'r', encoding='utf8')
+data = csv.reader(f)
+next(data)
+next(data)
+
+s_in = [0]*24 # 승차인원을 저장할 리스트 초기화
+s_out = [0]*24 # 하차인원을 저장할 리스트 초기화
+
+for row in data :
+    row[4:] = map(int, row[4:])
+    for i in range(24) :
+        s_in[i] += row[4+i*2] 
+        s_out[i] += row[5+i*2]
+        
+        
+plt.rc('font', family='AppleGothic')
+plt.title('지하철 시간대별 승하차 인원 추이') # 제목 추가
+plt.plot(s_in, label='승차') # 승차 인원을 꺾은 선 그래프로 표현
+plt.plot(s_out, label='하차') # 하차 인원을 꺾은 선 그래프로 표현
+plt.legend()
+plt.xticks(range(24), range(4,28))
+plt.show()
+
+
+# In[ ]:
+
+
+
+
---
+++ unit13-01.py
@@ -0,0 +1,320 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt 
+import numpy as np
+t = np.arange(0.,5.,0.2)
+plt.plot(t,t,'r--',t,t**2,'bs',t,t**3,'g^')
+plt.show()
+
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+t=[]
+p2=[]
+p3=[]
+for i in range(0,50,2):
+    t.append(i/10)
+    p2.append((i/10)**2)
+    p3.append((i/10)**3)
+plt.plot(t,t,'r--',t,p2,'bs',t,p3,'g^')
+plt.show()
+
+
+# In[3]:
+
+
+import numpy
+print(numpy.sqrt(2))
+
+
+# In[4]:
+
+
+import numpy as np
+print(np.sqrt(2))
+
+
+# In[5]:
+
+
+import numpy as np 
+print(np.pi)
+print(np.sin(0))
+print(np.cos(np.pi))
+
+
+# In[6]:
+
+
+import numpy as np
+a = np.random.rand(5)
+print(a)
+print(type(a))
+
+
+# In[7]:
+
+
+import numpy as np
+print(np.random.choice(6,10))
+
+
+# In[8]:
+
+
+import numpy as np
+print(np.random.choice(10,6,replace=False))
+
+
+# In[9]:
+
+
+import numpy as np
+print(np.random.choice(6, 10, p=[0.1, 0.2, 0.3, 0.2, 0.1, 0.1]))
+
+
+# In[10]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+dice = np.random.choice(6, 10)
+
+plt.hist(dice, bins =6)
+plt.show()
+
+
+# In[11]:
+
+
+import matplotlib.pyplot as plt
+import random
+dice = []
+for i in range(10) :
+    dice.append(random.randint(1,6))
+plt.hist(dice, bins = 6)
+plt.show()
+
+
+# In[16]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+dice = np.random.choice(6, 1000000, p=[0.1,0.2,0.3,0.2,0.1,0.1])
+plt.hist(dice, bins=6)  # 0, 1, 2, 3, 4, 5 중 랜덤으로 추출한 숫자를 히스토그램 표현
+plt.show()
+
+
+# In[24]:
+
+
+import matplotlib.pyplot as plt
+import random
+ 
+x = []
+y = []
+size = []
+ 
+for i in range(200) :
+    x.append(random.randint(10,100))
+    y.append(random.randint(10,100))
+    size.append(random.randint(10,100))
+ 
+
+plt.scatter(x, y, s=size, c=x, cmap='jet', alpha=0.7)
+plt.colorbar()
+plt.show()
+
+
+# In[25]:
+
+
+import numpy as np
+a = np.array([1,2,3,4])
+print(a)
+
+
+# In[26]:
+
+
+import numpy as np
+a = np.array([1,2,3,4])
+print(a[1], a[-1])  # a의 1번 인덱스 값, -1번 인덱스 값 출력
+print(a[1:])        # a의 1번 인덱스를 기준으로 슬라이싱 결과 출력
+
+
+# In[28]:
+
+
+import numpy as np
+a = np.array([1,2,'3',4])
+print(a)
+
+
+# In[29]:
+
+
+import numpy as np
+a = np.zeros(10)  # 0으로 이루어진 크기가 10인 배열 생성
+print(a)
+
+
+# In[30]:
+
+
+import numpy as np
+a = np.ones(10)   # 1로 이루어진 크기가 10인 배열 생성
+print(a)
+
+
+# In[31]:
+
+
+import numpy as np
+a = np.eye(3)  # 3행 x 3열의 단위 배열 생성
+print(a)
+
+
+# In[33]:
+
+
+import numpy as np
+a = np.eye(5)  # 5행 x 5열의 단위 배열 생성
+print(a)
+
+
+# In[34]:
+
+
+import numpy as np
+print(np.arange(3))     # arange( ) 함수에 1개 값 입력
+print(np.arange(3,7))   # arange( ) 함수에 2개 값 입력
+print(np.arange(3,7,2)) # arange( ) 함수에 3개 값 입력
+
+
+# In[35]:
+
+
+import numpy as np
+a = np.arange(1, 2, 0.1)   # 1이상 2미만 구간에서 0.1 간격으로 실수 생성
+b = np.linspace(1, 2, 11)  # 1부터 2까지 11개 구간으로 나눈 실수 생성
+print(a)
+print(b)
+
+
+# In[36]:
+
+
+import numpy as np
+a = np.arange(-np.pi, np.pi, np.pi/10)
+b = np.linspace(-np.pi, np.pi, 20)
+print(a)
+print(b)
+
+
+# In[37]:
+
+
+import numpy as np
+a = np.zeros(10) + 5
+print(a)
+
+
+# In[38]:
+
+
+import numpy as np
+a = np.linspace(1, 2, 11)
+print(np.sqrt(a))  # a값의 제곱근을 출력함
+
+
+# In[39]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+a = np.arange(-np.pi, np.pi, np.pi/100)
+plt.plot(a, np.sin(a))
+plt.show()
+
+
+# In[40]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+a = np.arange(-np.pi, np.pi, np.pi/100)
+plt.plot(a, np.sin(a))
+plt.plot(a, np.cos(a))
+plt.plot(a+np.pi/2, np.sin(a))
+plt.show()
+
+
+# In[41]:
+
+
+import numpy as np
+a = np.arange(-5, 5)
+print(a)
+
+
+# In[42]:
+
+
+print(a<0)
+
+
+# In[44]:
+
+
+import numpy as np
+a = np.arange(-5, 5)
+print(a[a<0])
+
+
+# In[45]:
+
+
+mask1 = abs(a) > 3 #abs(a) > 3은 a 배열에 저장된 원소의 절대값이 3보다 크다
+print(a[mask1])
+
+
+# In[46]:
+
+
+mask1 = abs(a) > 3
+mask2 = abs(a) % 2 == 0
+print(a[mask1+mask2])   # 둘 중 하나의 조건이라도 참일 경우
+print(a[mask1*mask2])   # 두 가지 조건이 모두 참일 경우
+
+
+# In[55]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.random.randint(-100, 100, 1000) # 1000개의 랜덤 값 추출
+y = np.random.randint(-100, 100, 1000) # 1000개의 랜덤 값 추출
+size = np.random.rand(100) * 100
+mask1 = abs(x) > 50            # x에 저장된 값 중 절댓값이 50보다 큰 값 걸러 냄
+mask2 = abs(y) > 50            # y에 저장된 값 중 절댓값이 50보다 큰 값 걸러 냄
+x = x[mask1+mask2]             # mask1과 mask2 중 하나라도 만족하는 값 저장
+y = y[mask1+mask2]  # mask1과 mask2 중 하나라도 만족하는 값 저장
+size = np.random.rand(len(x)) * 100
+plt.scatter(x, y, s=size, c=x, cmap='jet', alpha=0.7)
+plt.colorbar()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
